8c0992c241ff8deecde79b348f0cb111c5588197
Edwin <zizo_edwin@hotmail.com>
2022-12-06 08:37:23 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const indexRouter = require("./routes/index");
+const PORT = 3001;
+// Start the server
+const app = express();
+app.use(cors());
+app.use("/", indexRouter);
+app.use('/public', express.static(__dirname + '/public'));
+
+app.listen(PORT, () => {
+  console.log(`listening on port :${PORT}`);
+});
